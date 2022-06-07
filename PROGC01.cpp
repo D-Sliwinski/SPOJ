4fe2aa66eab5c1cea37f293a1faf1868f129c9a0
D-Sliwinski <107050274+D-Sliwinski@users.noreply.github.com>
2022-06-07 16:17:28 +0200

    Add PROGC01

---
+++ PROGC01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(){
+	int linie=0;
+    string zlicz;
+    ifstream plik("Plik tekstowy.txt");
+    while(getline(cin, zlicz)){
+    	linie++;
+	}
+    cout << linie;
+    plik.close();
+}
