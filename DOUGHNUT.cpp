cb55cebbce5437644f8fa251472708983af3c21b
D-Sliwinski <107050274+D-Sliwinski@users.noreply.github.com>
2022-06-07 15:29:27 +0200

    Add DOUGHNUT

---
+++ DOUGHNUT.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int c,k,w;
+	int t;
+	cin >> t;
+	while(t--){
+		cin >> c >> k >> w;
+		if(c*w<=k){ 
+			cout << "yes" << endl;
+		}
+		else{
+			cout << "no" << endl;
+		}
+	}
+}
