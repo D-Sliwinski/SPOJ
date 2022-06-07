e62d4b696fff9461184c1e9695def2d52eedaf83
D-Sliwinski <107050274+D-Sliwinski@users.noreply.github.com>
2022-06-07 15:48:42 +0200

    Add FR_14_02

---
+++ FR_14_02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+	string n,n2="sto";
+	int x;
+	cin >> n;
+	x=n.find(n2);
+	if(x!=-1){
+		cout << "TAK";
+	}
+	else{
+		cout <<"NIE";
+	}
+}
