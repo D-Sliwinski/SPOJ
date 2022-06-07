a473a49afec1200d0bf229c9424eb7baca5486a9
D-Sliwinski <107050274+D-Sliwinski@users.noreply.github.com>
2022-06-07 15:20:25 +0200

    Add AL_17_01

---
+++ AL_17_01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string n="Mundial ";
+	string a="aa";
+	string b="";
+	string c="a";
+	string d="aaaa";
+	cout << n << a.length() << b.length() << c.length()<< d.length();
+}
