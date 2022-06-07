9f05ff83bbf46f90285c4301390afdd8a580c066
D-Sliwinski <107050274+D-Sliwinski@users.noreply.github.com>
2022-06-07 15:46:59 +0200

    Add FR_12_02

---
+++ FR_12_02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string n,n2;
+	cin >> n >> n2;
+	if(n[0]==n2[0]||n[1]==n2[1]){
+		cout << "TAK" << endl;
+	}
+	else{
+		cout << "NIE" << endl;
+	}
+}	
+
